d83a7093b2255295cf8e0b0dd996bcef0292d518
eytyy <e.tayyem@gmail.com>
2025-06-22 20:17:20 +0300

    fix padding

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -3,7 +3,7 @@ import Ticker from './Ticker';
 
 export default function Hero() {
   return (
-    <div className="hero min-h-svh lg:h-screen grid grid-rows-[1fr_min-content] items-center justify-center bg-white relative border-b border-b-black dark:bg-black dark:border-b-white w-full pt-[132px]">
+    <div className="hero min-h-svh lg:h-screen grid grid-rows-[1fr_min-content] items-center justify-center bg-white relative border-b border-b-black dark:bg-black dark:border-b-white w-full pt-[132px] lg:pt-0">
       <HeroLogo />
       <Ticker />
     </div>
