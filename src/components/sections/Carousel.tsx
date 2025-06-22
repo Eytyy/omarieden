412c8361d73a1b7082d94567a8a196d809b3d16b
eytyy <e.tayyem@gmail.com>
2025-06-22 22:49:49 +0300

    line height

--- src/components/sections/Carousel.tsx
+++ src/components/sections/Carousel.tsx
@@ -54,7 +54,7 @@ export default function Carousel({ title, products }: { title: string; products:
 function SectionHeader({ title }: { title: string }) {
   return (
     <header className="w-1/2 lg:w-auto px-4 pt-8 lg:grid lg:grid-cols-3 lg:px-0 2xl:grid-cols-4">
-      <div className="text-lg uppercase opacity-50 dark:opacity-80 lg:col-start-2 xl:px-4 lg:px-8">
+      <div className="text-lg uppercase opacity-50 dark:opacity-80 lg:col-start-2 xl:px-4 lg:px-8 leading-[1.1] mb-1">
         {title}
       </div>
       <a href="#" className="text-sm xl:px-4 lg:col-start-2 lg:px-8">
